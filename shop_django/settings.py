1097fc40055b953209bffc14443ee88a63deaf45
Sava <sava777cool@gmail.com>
2021-07-28 14:23:20 +0300

    Comment STATICFILES_DIRS

--- shop_django/settings.py
+++ shop_django/settings.py
@@ -134,14 +134,14 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/  os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mainapp/static/'),
-]
+#STATICFILES_DIRS = [
+    #os.path.join(BASE_DIR, 'mainapp/static/'),
+#]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
