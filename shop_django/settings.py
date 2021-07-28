5cae497688ddaf1da20804ea30cf3c6d73f9eed6
Sava <sava777cool@gmail.com>
2021-07-28 14:01:58 +0300

    Add to setting.py import django_heroku

--- shop_django/settings.py
+++ shop_django/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 import os
 import sys
 import dj_database_url
-
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
